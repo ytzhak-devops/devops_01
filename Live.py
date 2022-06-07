a454fe9a1aa61df875b20d72f6cd0f6b0fdc6f8f
ytzhak <netpro4you@gmail.com>
2022-06-07 16:07:20 -0400

    live file added

---
+++ Live.py
@@ -0,0 +1,7 @@
+def welcome(name):
+    username = input('Please enter your name: ')
+    print(f"Hello {username} and welcome to the World of Games(WoG)\
+          \nHere you can find many cool games to play.")
+
+
+welcome("username")
