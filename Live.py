fd3684c0de4500b9b9d7ef64df4dc527a10cf91a
ytzhak <netpro4you@gmail.com>
2022-06-07 17:14:26 -0400

    edit 3

--- Live.py
+++ Live.py
@@ -1,7 +1,41 @@
-def welcome(name):
-    username = input('Please enter your name: ')
-    print(f"Hello {username} and welcome to the World of Games(WoG)\
-          \nHere you can find many cool games to play.")
+# def welcome(name):
+#     username = input('Please enter your name: ')
+#     print(f"Hello {username} and welcome to the World of Games(WoG)\
+#           \nHere you can find many cool games to play.")
+#
+#
+# welcome("username")
+#
+# def menu():
+#     print("[1] Option 1")
+#     print("[2] Option 2")
+#     print("[0] Exit the program.")
+# menu()
+# option = int(input("Enter your option: "))
+#
+# while option != 0:
+#     if option == 1:
+#         #do option 1 stuff
+#         print("Option 1 has been called.")
+#     elif option == 2:
+#         #do option 2 stuff
+#         print("Option 2 has been called.")
+#     else:
+#         print("Invalid option . ")
+#     print()
+#     menu()
+#     option = int(input("Enter your option: "))
+# print("Thanks for using this program. Goodbye.")
 
 
-welcome("username")
+choice = input("""
+hello enter you coislsl
+a - memoty.lsdfjs
+b - lslsl.sldfjl
+c - sldfjlsd.sfllsdfjlsf
+make a selection : 
+""")
+
+
+https://www.youtube.com/watch?v=P6azEyNIQDQ
+https://www.youtube.com/watch?v=TRaXgtTauuE
