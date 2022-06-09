e85db75557b0e1947b2ce59dd286956f65a3abf4
ytzhak-devops <107072401+ytzhak-devops@users.noreply.github.com>
2022-06-09 08:44:02 +0300

    Update Live.py

--- Live.py
+++ Live.py
@@ -10,17 +10,17 @@ welcome("username")
 def load_game():
     choice = input("""
     Please choose a game to play:
-    
+
     1. Memory Game        - a sequence of numbers will appear for 1 second and you have to guess it back
     2. Guess Game         - guess a number and see if you chose like the computer
     3. Currency Roulette  - try and guess the value of a random amount of USD in ILS
-    
-    make a selection : 
+
+    make a selection :
     """)
 
-    if 1 <= choice.isdigit() <= 3:
+    if choice.isdigit() and (1 <= int(choice) <= 3):
         difficulty = input('Please choose game difficulty from 1 to 5 : ')
-        if 1 <= difficulty.isdigit() <= 5:
+        if difficulty.isdigit() and (1 <= int(difficulty) <= 5):
             print(f'You choose {difficulty} level , good luck...')
         else:
              print("Not a valid number ! Try again...")
@@ -28,4 +28,5 @@ def load_game():
         print("Not a valid number ! Try again...")
 
 load_game()
+
 
