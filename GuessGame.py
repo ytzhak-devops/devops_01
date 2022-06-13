555b59783af895cd5eac7c28471e538408705e54
ytzhak-devops <107072401+ytzhak-devops@users.noreply.github.com>
2022-06-13 11:22:40 +0300

    Update GuessGame.py

--- GuessGame.py
+++ GuessGame.py
@@ -4,21 +4,21 @@ import random
 
 difficulty = True
 while difficulty:
-    bom = input('Enter your difficulty level : ')
-    if bom.isdigit():
+    upper_number = input('Enter the upper number : ')
+    if upper_number.isdigit():
         print("Let's Play ! ")
-        bom = int(bom)
+        upper_number = int(upper_number)
         difficulty = False
     else:
         print("Invalid input! Try again... ")
 
-secret_number = random.randint(1, bom)
+secret_number = random.randint(1, upper_number)
 
 guess = None
 count = 1
 
 while guess != secret_number:
-    guess = input("Please type a number between 1 and " + str(bom) + ":" )
+    guess = input("Please type a number between 1 and " + str(upper_number) + ":" )
     
         
               
@@ -32,3 +32,7 @@ while guess != secret_number:
         
 print("It took " , count , "guesses ", "Goodbye...")
 
+      
+
+
+            
